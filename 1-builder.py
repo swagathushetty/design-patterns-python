099823e74489ddbd6662a6da013efc2c9069df62
swagath shetty <swagath.shetty@gmail.com>
2024-06-16 21:46:32 +0530

    added game builder calss

--- 1-builder.py
+++ 1-builder.py
@@ -200,11 +200,59 @@ class GameObject:
     def __init__(self):
         self.transform:Transform=None
         self.renderer:Renderer=None
-        self.collider:Colllider=None
+        self.collider:Collider=None
         self.script:str=None
     def __str__(self) -> str:
         return f"Transform: {self.transform}\n"\
             f"Renderer: {self.renderer}\n"\
             f"Collider: {self.collider}\n"\
-            f"Script: {self.script}\n"
+            f"Screipt: {self.script}\n"
+    
+class Transform:
+    def __init__(self,position:tuple,rotation:tuple,scale:tuple):
+        self.position = position
+        self.rotation = rotation
+        self.scale = scale
+    def __str__(self):
+           return f"Postion: {self.position}\n"\
+            f"Rotation: {self.rotation}\n"\
+            f"Scale: {self.scale}\n" 
+class Renderer:
+    def __init__(self,mesh:str,material:str):
+        self.mesh = mesh
+        self.material = material
+    def __str__(self):
+        return f"Mesh: {self.mesh}, Material:{self.material}"
+    
+class Collider:
+    def __init__(self,shape:str,is_trigger:str):
+        self.shape = shape
+        self.is_trigger = is_trigger
+    def __str__(self):
+        return f"Shape: {self.shape}, Trigger:{self.is_trigger}"
+    
+class GameBuilder:
+    def __init__(self):
+        self._game = GameObject()
+    def add_transform(self,position,rotation,scale):
+        self._game.transform = Transform(position,rotation,scale)
+    def add_renderer(self,mesh,material):
+        self._game.renderer = Renderer(mesh,material)
+    def add_collider(self,shape,is_trigger):
+        self._game.collider = Collider(shape,is_trigger)
+    def add_script(self,script):
+        self._game.script = (script)
+    def get_game(self):
+        return self._game 
 
+print('===================== ENGINE 1 ===============================')
+
+builder = GameBuilder()
+builder.add_transform((1,2,4),(4,6,2),(5,6,2))
+builder.add_renderer("cubeMesh","DefaultBlackShine")
+builder.add_collider("Box",False)
+builder.add_script("TurnScript.js")
+
+
+game1 = builder.get_game()
+print(game1)
